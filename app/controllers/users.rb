ccc8f9648ef7e3d909dd0f3b468718d4c35f3576
Aminah Burch <a.j.burch11@gmail.com>
2017-10-29 15:39:50 -0500

    Added logout routes and ability for newly registered user to be auto logged in; errors partial, nav links, registration form

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,7 +6,8 @@ post '/users' do
   @user = User.new(params[:user])
 
   if @user.save
-    redirect '/users'
+    set_current_user_id(@user.id)
+    redirect '/'
   else
     @errors = @user.errors.full_messages
     erb :'/users/new'
