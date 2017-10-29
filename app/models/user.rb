c9ccc235003520c19c8aaca49c592f3adddfc6f4
Aminah Burch <a.j.burch11@gmail.com>
2017-10-29 13:52:20 -0500

    Added all models with associations and user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :comments
+  has_many :testimonials
+
+  validates :first_name, :last_name, :email, { presence: true }
+  validates :email, { uniqueness: true }
+  validate :validate_password
+
+  #-------------------USER AUTHENTICATION-------------------
+
+  def password
+    @password ||= BCrypt::Password.new(secretized_password)
+  end
+
+  def password=(plain_text_password)
+    @raw_password = plain_text_password
+    @password = BCrypt::Password.create(plain_text_password)
+    self.secretized_password = @password
+  end
+
+  def authenticate(plain_text_password)
+    self.password == plain_text_password
+  end
+
+  def validate_password
+    if @raw_password == ""
+      errors.add(:password, "is require.")
+    elsif @raw_password.length < 6
+      errors.add(:password, "must be at least 6 characters.")
+    end
+  end
 end
