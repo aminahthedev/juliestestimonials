256566b0297d1df832bdb3096a667a713d747649
Aminah Burch <a.j.burch11@gmail.com>
2017-11-08 14:04:19 -0600

    Tried to add the api key for giphy, will add as a later component

--- config/environment.rb
+++ config/environment.rb
@@ -49,3 +49,13 @@ Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 # Set up the database and models
 require APP_ROOT.join('config', 'database')
+
+# config_files = ['secrets.yml']
+#
+#   config_files.each do |file_name|
+#     file_path = File.join(Rails.root, 'config', file_name)
+#     config_keys = HashWithIndifferentAccess.new(YAML::load(IO.read(file_path)))[Rails.env]
+#     config_keys.each do |k, v|
+#       ENV[k.upcase] ||= v
+#     end
+#   end
